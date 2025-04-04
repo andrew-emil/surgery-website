bca4b3f4a987fd6cc5faccfb7fcac480d6d285a8
unknown <adhmdoma11@gmail.com>
2025-04-05 01:14:29 +0200

    fixing bug of user cookie

--- frontend/src/views/OTP_auth.jsx
+++ frontend/src/views/OTP_auth.jsx
@@ -10,93 +10,103 @@ import { Navigate } from "react-router-dom";
 import * as jose from "jose";
 
 export default function OTP_auth() {
-	const [otp, setOtp] = useState("");
-	const [err, setErr] = useState(null);
-	const [isLoading, setIsLoading] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [err, setErr] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-	const StyledImg = styled("img")(({ theme }) => ({
-		width: 100,
-		height: 100,
-		borderRadius: "50%",
-		objectFit: "contain",
-		border: `2px solid ${theme.palette.primary.main}`,
-		boxShadow: theme.shadows[2],
-		marginBottom: ".5rem",
-	}));
-	const { setUser, setToken, user } = useStateContext();
+  const StyledImg = styled("img")(({ theme }) => ({
+    width: 100,
+    height: 100,
+    borderRadius: "50%",
+    objectFit: "contain",
+    border: `2px solid ${theme.palette.primary.main}`,
+    boxShadow: theme.shadows[2],
+    marginBottom: ".5rem",
+  }));
+  const { setUser, setToken, user } = useStateContext();
 
-	if (!user) {
-		return <Navigate to="/login" />;
-	}
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
 
-	// const secretKey = import.meta.env.VITE_JWT_SECRET;
-	const secret = new TextEncoder().encode("mySecret1243");
-	const submit = (ev) => {
-		ev.preventDefault();
-		setIsLoading(true);
-		setErr("");
-		const payload = {
-			email: user,
-			otp: otp,
-		};
+  const secret = new TextEncoder().encode("mySecret1243");
+  const submit = (ev) => {
+    ev.preventDefault();
+    setIsLoading(true);
+    setErr("");
+    const payload = {
+      email: user,
+      otp: otp,
+    };
 
-		axiosClient
-			.post("/users/verify", payload)
-			.then(({ data }) => {
-				const token = data.token;
-				setToken(token);
-				return jose.jwtVerify(token, secret, { algorithms: ["HS256"] });
-			})
-			.then((result) => {
-				console.log("Decoded Payload:", result.payload);
-				setUser(result.payload); // Set user from verified payload
-			})
-			.catch((err) => {
-				console.error("Error:", err);
-				setErr(err.response?.data?.message || "Verification failed");
-			})
-			.finally(() => setIsLoading(false));
-	};
+    axiosClient
+      .post("/users/verify", payload)
+      .then(({ data }) => {
+        const token = data.token;
+        jose
+          .jwtVerify(token, secret, { algorithms: ["HS256"] })
+          .then((result) => {
+            setUser(result.payload);
+            setToken(token);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response) {
+          setErr(response.data.message);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-	return (
-		<FormContainer>
-			<FormCard
-				sx={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					padding: "3rem",
-					width: "28rem",
-				}}>
-				{err && (
-					<Alert severity="error" sx={{ marginBottom: "1rem", width: "26rem" }}>
-						<AlertTitle>Error</AlertTitle>
-						{err}
-					</Alert>
-				)}
-				<StyledImg
-					sx={{ marginTop: "2rem" }}
-					src="/images/Otp_Icon.svg"></StyledImg>
-				<FormTitle>Verfication Code</FormTitle>
-				<Typography
-					variant="body2"
-					className="message"
-					sx={{ textAlign: "center", marginBottom: "1rem" }}>
-					We have sent a verification code to your email address. Please enter
-					the code to verify your account.
-				</Typography>
-				<form onSubmit={submit} action="">
-					<OTPInput otp={otp} setOtp={setOtp}></OTPInput>
-					<FormButton
-						type="submit"
-						variant="contained"
-						className="btn btn-black btn-block"
-						sx={{ width: "100%", marginTop: "1rem", marginBottom: "2rem" }}
-						loading={isLoading}>
-						Submit
-					</FormButton>
-				</form>
-			</FormCard>
-		</FormContainer>
-	);
+  return (
+    <FormContainer>
+      <FormCard
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "3rem",
+          width: "28rem",
+        }}
+      >
+        {err && (
+          <Alert severity="error" sx={{ marginBottom: "1rem", width: "26rem" }}>
+            <AlertTitle>Error</AlertTitle>
+            {err}
+          </Alert>
+        )}
+        <StyledImg
+          sx={{ marginTop: "2rem" }}
+          src="/images/Otp_Icon.svg"
+        ></StyledImg>
+        <FormTitle>Verfication Code</FormTitle>
+        <Typography
+          variant="body2"
+          className="message"
+          sx={{ textAlign: "center", marginBottom: "1rem" }}
+        >
+          We have sent a verification code to your email address. Please enter
+          the code to verify your account.
+        </Typography>
+        <form onSubmit={submit} action="">
+          <OTPInput otp={otp} setOtp={setOtp}></OTPInput>
+          <FormButton
+            type="submit"
+            variant="contained"
+            className="btn btn-black btn-block"
+            sx={{ width: "100%", marginTop: "1rem", marginBottom: "2rem" }}
+            loading={isLoading}
+          >
+            Submit
+          </FormButton>
+        </form>
+      </FormCard>
+    </FormContainer>
+  );
 }
